ff3de1e56511180afd80301b06805aed10620809
Mike Lee <iskitz@users.noreply.github.com>
2019-10-18 06:17:49 +0900

    tests: readme: load readme code sample ions to sense regressions

--- tools/tests.js
+++ tools/tests.js
@@ -5,12 +5,12 @@
     , by: ['mike.lee', 'team']
     , in:  'san-jose.california.usa.earth'
     , on:  -7.20160518
-    , to:  -7.20190428
+    , to:  -7.20190925
     , is:  -0.1
     , it:  "tests ion & ionify js"
-    , we:
-        [" were testing ionify's emoji actions: ['0 . 0'] + ['0 . -'] & ['- . -'] "
-        ]
+    , we: ["were adding readme tests"
+          ,"like testing ionify's emoji actions: ['0 . 0'] + ['0 . -'] & ['- . -']"
+          ]
     }
 }
 +
@@ -18,7 +18,16 @@
 :   [             'on.ion.test@ionify',       'on.test@ionify',        'do.test@ionify'
     ,                'get.test@ionify',      'log.test@ionify',       'use.test@ionify'
     , 'on.ion.punctuation.test@ionify', 'on.emoji.test@ionify', 'on.errors.test@ionify'
-    ,               'find.test@ionify',     'opus.test@ionify'
-    ]
+    ,               'find.test@ionify',
+    , 'readme.intro@ionify'
+    , 'readme.ask.say@ionify'
+    , 'readme.literate@ionify'
+    , 'readme.code.prose@ionify'
+    , 'readme.decoupled@ionify'
+    , 'readme.event.driven@ionify'
+    , 'readme.modular@ionify'
+    , 'readme.duplicate.ids@ionify'
+    ],
+  in: 'order'
 }
 ;
